6a8390767e8e01156d9a08075e4f6b50d3ddfa4d
Ahmet <ankakb27@gmail.com>
2023-04-09 10:40:55 +0300

    commit2

--- Source/UE_Task01/Private/Cell.cpp
+++ Source/UE_Task01/Private/Cell.cpp
@@ -2,13 +2,21 @@
 
 
 #include "Cell.h"
-
+#include "Components/SphereComponent.h"
 // Sets default values
 ACell::ACell()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	ColliderComp= CreateDefaultSubobject<USphereComponent>("ColliderComp");
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	RootComponent = MeshComponent;
+	ColliderComp->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMesh(TEXT("/Engine/BasicShapes/Sphere"));
+	if (SphereMesh.Succeeded())
+	{
+		MeshComponent->SetStaticMesh(SphereMesh.Object);
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/UE_Task01/Public/Cell.h
+++ Source/UE_Task01/Public/Cell.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Actor.h"
 #include "Cell.generated.h"
 
@@ -18,7 +19,13 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	
+	UPROPERTY(EditAnywhere)
+	USphereComponent* ColliderComp;
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComponent;
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
